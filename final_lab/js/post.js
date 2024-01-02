e78df5f781358cd9eea7655cc0585736eb62d233
Kiameow <zjzootl@qq.com>
2024-01-02 20:52:49 +0800

    fix: post bug

--- final_lab/js/post.js
+++ final_lab/js/post.js
@@ -2,37 +2,35 @@ const submitBtn = document.getElementById('form-submit');
     submitBtn.addEventListener('click', function(event) {
       event.preventDefault();
 
-      validateForm();
+      if (validateForm()) {
+        const title = document.getElementById('title').value;
+        const description = document.getElementById('description').value;
+        const category = document.getElementById('book').value ? 'book' : 'movie';
+        const tags = [];
+        tags.push(document.getElementById('tags').value);
+        const imgURL = document.getElementById('img-url').value;
+        const content = document.getElementById('post-content').value;
 
-      const title = document.getElementById('title').value;
-      const description = document.getElementById('description').value;
-      const category = document.getElementById('book').value ? 'book' : 'movie';
-      const tags = [];
-      tags.push(document.getElementById('tags').value);
-      const imgURL = document.getElementById('img-url').value;
-      const content = document.getElementById('post-content').value;
+        const articles = localStorage.getItem('articles');
+        const articlesData = JSON.parse(articles);
+        
+        console.log(articlesData);
+    
+        const id = getLength(articlesData[category]) + 1;
+        const newArticle = {
+          'id': id,
+          'title': title,
+          'description': description,
+          'tags': tags,
+          'imgURL': imgURL,
+          'content': content
+        }
+        articlesData[category].push(newArticle);
 
-      const articles = localStorage.getItem('articles');
-      const articlesData = JSON.parse(articles);
-      
-      console.log(articlesData);
-  
-      const id = getLength(articlesData[category]) + 1;
-      const newArticle = {
-        'id': id,
-        'title': title,
-        'description': description,
-        'tags': tags,
-        'imgURL': imgURL,
-        'content': content
+        console.log(articlesData);
+
+        localStorage.setItem('articles', JSON.stringify(articlesData));
       }
-      articlesData[category].push(newArticle);
-
-      console.log(articlesData);
-
-      localStorage.setItem('articles', JSON.stringify(articlesData));
-
-      
     })
 
 function getLength(jsonData) {
@@ -59,20 +57,21 @@ function validateForm() {
 
   if (titleValue === '') {
     showNotification('标题不能为空', 'red');
-    return;
+    return false;
   }
 
   if (descValue === '') {
-    showNotification('标题不能为空', 'red');
-    return;
+    showNotification('副标题不能为空', 'red');
+    return false;
   }
 
   const specialChars = /[!@#$%^&*(),.?":{}|<>]/;
-  if (specialChars.test(titleValue)) {
-    showNotification('标题不能包含特殊符号', 'red');
-    return;
+  if (specialChars.test(titleValue) || specialChars.test(descValue)) {
+    showNotification('字段不能包含特殊符号', 'red');
+    return false;
   }
 
   showNotification('提交成功！', 'green');
   // 可以继续执行其他操作
+  return true;
 }
